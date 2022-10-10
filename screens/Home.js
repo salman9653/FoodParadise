6596538cee5dc50af8bf5c33ce6e2fbfc9e5b58f
salman9653 <salman9915189734@gmail.com>
2022-10-10 11:52:43 +0530

    Added Header and Search Bar

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,8 @@
 import { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Image, SafeAreaView } from 'react-native';
+import { View, Text, Image, SafeAreaView, TextInput } from 'react-native';
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from 'react-native-heroicons/outline';
+
 import SafeViewAndroid from "./components/SafeViewAndroid";
 
 function Home() {
@@ -15,13 +17,34 @@ function Home() {
 
     return (
         <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-            <View>
+
+            <View className="flex-row pb-3 mt-2 items-center mx-4 space-x-2">
                 <Image
                     source={require('../assets/delivery-bike.png')}
                     className="h-7 w-7 bg-gray-300 p-4 rounded-full"
                 />
+                <View className="flex-1">
+                    <Text className="font-bold text-gray-400 text-xs">
+                        Deliver Now!
+                    </Text>
+                    <Text className="font-bold text-xl">
+                        Current Location
+                        <ChevronDownIcon size={20} color="#00CC88" />
+                    </Text>
+                </View>
+                <UserIcon size={35} color="#00CCBB" />
             </View>
-        </SafeAreaView>
+
+
+            <View className="flex-row items-center space-x-2s pb-2 mx-4">
+                <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                    <UserIcon size={20} color="gray" />
+                    <TextInput placeholder='Restaurants or cuisines' keyboardType='default' />
+                </View>
+                <ChevronDownIcon color="#00CCBB" />
+            </View>
+
+        </ SafeAreaView>
     );
 }
 
