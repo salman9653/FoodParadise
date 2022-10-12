03e6bd17ebf5488343f414cde72aee7c4fbe30da
salman9653 <salman9915189734@gmail.com>
2022-10-12 13:25:38 +0530

    added schemas

---
+++ sanity/schemas/restaurant.js
@@ -0,0 +1,59 @@
+export default {
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Restaurant name',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'short_description',
+      type: 'string',
+      title: 'Short description',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Restaurant image',
+    },
+    {
+      name: 'lat',
+      type: 'number',
+      title: 'Latitude of the restaurant',
+    },
+    {
+      name: 'long',
+      type: 'number',
+      title: 'Longitude of the restaurant',
+    },
+    {
+      name: 'address',
+      type: 'string',
+      title: 'Restaurant address',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'rating',
+      type: 'number',
+      title: 'Ratings from (1-5 Stars)',
+      validation: (Rule) => Rule.required().min(1).max(5).error("Please enter a value between 1 and 5"),
+    },
+    {
+      name: 'type',
+      title: 'Category',
+      type: 'reference',
+      to: [{ type: 'category' }],
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'dishes',
+      type: 'array',
+      title: 'Dishes',
+      of: [{ type: 'reference', to: [{ type: "dish" }] }],
+    }
+  ],
+}
