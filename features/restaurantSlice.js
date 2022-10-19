c5e17b40e6514e635ae064cdbf739c5eff5aed2c
salman9653 <salman9915189734@gmail.com>
2022-10-19 18:57:36 +0530

    error fixed

--- features/restaurantSlice.js
+++ features/restaurantSlice.js
@@ -6,26 +6,26 @@ const initialState = {
         imgUrl: null,
         title: null,
         rating: null,
-        enre: null,
+        genre: null,
         address: null,
         short_description: null,
         dishes: null,
-    }
+    },
 }
 
 export const restaurantSlice = createSlice({
     name: 'restaurant',
     initialState,
     reducers: {
-        setRestautant: (state, action) => {
+        setRestaurant: (state, action) => {
             state.restaurant = action.payload;
         }
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { setRestautant } = restaurantSlice.actions;
+export const { setRestaurant } = restaurantSlice.actions;
 
-export const selectRestaurant = state => state.restaurant.restaurant;
+export const selectRestaurant = state => state.restaurant.restaurant
 
-export default restaurantSlice.reducer
+export default restaurantSlice.reducer;
